c8baec94d6c1a34fba6f81be94ecf1de2cd36727
feisalmombo <feisalmombo29@gmail.com>
2023-02-28 11:36:13 +0300

    WIP

--- resources/views/pages/air/airfreight.blade.php
+++ resources/views/pages/air/airfreight.blade.php
@@ -3,30 +3,29 @@
 <html>
 
 <head>
-
- <title>Iwe Freight - Clearing and forwarding company in East Africa</title>
-        <meta name="description" content="Iwe Freight - Clearing and forwarding company in East Africa">
-        <meta name="author" content="Iwe Freight">
+        <title>Giant Freight Limited - Air Freight</title>
+        <meta name="description" content="Giant Freight">
+        <meta name="author" content="Giant Freight">
         <meta name="keywords" content="transportation, logistics, transportation , logistics , cargo, business">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Stylesheets -->
-        <link rel="stylesheet" href="css/bootstrap.css"/><!-- bootstrap grid -->
-        <link rel="stylesheet" href="css/animate.css"/><!-- used for animations -->
-        <link rel='stylesheet' href='owl-carousel/owl.carousel.css'/><!-- Client carousel -->
-        <link rel="stylesheet" href="masterslider/style/masterslider.css" /><!-- Master slider css -->
-        <link rel="stylesheet" href="masterslider/skins/default/style.css" /><!-- Master slider default skin -->
-        <link rel="stylesheet" href="css/style.css"/><!-- template styles -->
-        <link rel="stylesheet" href="css/color-default.css"/><!-- template main color -->
-        <link rel="stylesheet" href="css/retina.css"/><!-- retina ready styles -->
-        <link rel="stylesheet" href="css/responsive.css"/><!-- responsive styles -->
+        <link rel="stylesheet" href="{{asset('giant/css/bootstrap.css')}}"/><!-- bootstrap grid -->
+        <link rel="stylesheet" href="{{asset('giant/css/animate.css')}}"/><!-- used for animations -->
+        <link rel='stylesheet' href="{{asset('giant/owl-carousel/owl.carousel.css')}}"/><!-- Client carousel -->
+        <link rel="stylesheet" href="{{asset('giant/masterslider/style/masterslider.css')}}" /><!-- Master slider css -->
+        <link rel="stylesheet" href="{{asset('giant/masterslider/skins/default/style.css')}}" /><!-- Master slider default skin -->
+        <link rel="stylesheet" href="{{asset('giant/css/style.css')}}"/><!-- template styles -->
+        <link rel="stylesheet" href="{{asset('giant/css/color-default.css')}}"/><!-- template main color -->
+        <link rel="stylesheet" href="{{asset('giant/css/retina.css')}}"/><!-- retina ready styles -->
+        <link rel="stylesheet" href="{{asset('giant/css/responsive.css')}}"/><!-- responsive styles -->
 
         <!-- Google Web fonts -->
         <link href='http://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,800,700,600' rel='stylesheet' type='text/css'>
 
         <!-- Font icons -->
-        <link rel="stylesheet" href="icon-fonts/font-awesome-4.3.0/css/font-awesome.min.css"/><!-- Fontawesome icons css -->
+        <link rel="stylesheet" href="{{asset('giant/icon-fonts/font-awesome-4.3.0/css/font-awesome.min.css')}}"/><!-- Fontawesome icons css -->
 </head>
 
     <body>
