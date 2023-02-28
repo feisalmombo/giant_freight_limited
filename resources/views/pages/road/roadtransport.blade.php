37e877640658647d6233ddff4b54c00ad2e4dcb8
feisalmombo <feisalmombo29@gmail.com>
2023-02-28 11:38:28 +0300

    WIP

--- resources/views/pages/road/roadtransport.blade.php
+++ resources/views/pages/road/roadtransport.blade.php
@@ -145,7 +145,7 @@
                                     <a href="{{ url('/') }}">Home</a>
                                 </li>
                                 <li>
-                                    <a href="{{ url('/') }}">Services</a>
+                                    <a href="{{ url('/') }}">Our Services</a>
                                 </li>
 
                                 <li>
