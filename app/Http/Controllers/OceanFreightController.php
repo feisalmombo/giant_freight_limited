91bbfb67a7ab2856d6e37f7c69193d87594cb8c4
feisalmombo <feisalmombo29@gmail.com>
2023-03-01 14:04:48 +0300

    WIP

--- app/Http/Controllers/OceanFreightController.php
+++ app/Http/Controllers/OceanFreightController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class OceanFreightController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('pages.ocean.oceanfreight');
+    }
 }
