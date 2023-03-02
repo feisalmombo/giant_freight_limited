30d61d2e5e1b778574e2a8c02179f1275ff52e77
feisalmombo <feisalmombo29@gmail.com>
2023-03-02 15:25:55 +0300

    WIP

--- app/Http/Controllers/RailServicesController.php
+++ app/Http/Controllers/RailServicesController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class RailServicesController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('pages.rail.railservices');
+    }
 }
---
+++ resources/views/pages/rail/railservices.blade.php
@@ -0,0 +1,1 @@
+<h1>RAIL SERVICES</h1>
