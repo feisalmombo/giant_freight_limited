6719c1ad6bb0d92481c3e4f19e0f42dfe6e7ebcf
feisalmombo <feisalmombo29@gmail.com>
2023-03-04 12:44:35 +0300

    WIP

--- resources/views/pages/rail/railservices.blade.php
+++ resources/views/pages/rail/railservices.blade.php
@@ -62,7 +62,7 @@
                                                 <ul class="dropdown-menu">
                                                     <li><a href="{{url('/road-transport')}}">Road Transport</a></li>
                                                     <li><a href="{{ url('/air-freight') }}">Air Freight</a></li>
-                                                    <li><a href="#">Ocean Freight</a></li>
+                                                    <li><a href="{{ url('/ocean-freight') }}">Ocean Freight</a></li>
                                                     <li><a href="#">Customs Clearance</a></li>
                                                     <li><a href="#">Warehousing</a></li>
                                                     <li><a href="#">Value Added Servives</a></li>
@@ -99,7 +99,7 @@
                                                             <ul class="dl-submenu">
                                                                 <li><a href="{{url('/road-transport')}}">Road Transport</a></li>
                                                                 <li><a href="{{url('/air-freight')}}">Air Freight</a></li>
-                                                                <li><a href="#">Ocean Freight</a></li>
+                                                                <li><a href="{{ url('/ocean-freight') }}">Ocean Freight</a></li>
                                                                 <li><a href="#">Customs Clearance</a></li>
                                                                 <li><a href="#">Warehousing</a></li>
                                                                 <li><a href="#">Value Added Servives</a></li>
@@ -173,7 +173,7 @@
                                     </li>
 
                                     <li class="menu-item current-menu-item">
-                                        <a href="#">Ocean Freight</a>
+                                        <a href="{{ url('/ocean-freight') }}">Ocean Freight</a>
                                     </li>
 
                                     <li class="menu-item">
