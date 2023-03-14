07481b04c97e738d7ea9184b6e58aef2d6e8af84
feisalmombo <feisalmombo29@gmail.com>
2023-03-14 15:36:19 +0300

    WIP

--- app/Http/Controllers/WarehousingController.php
+++ app/Http/Controllers/WarehousingController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class WarehousingController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('pages.warehouse.warehousing');
+    }
 }
