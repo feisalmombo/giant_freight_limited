0f0a0c80aee8c2529e1e927fcbb47859ef7e8818
feisalmombo <feisalmombo29@gmail.com>
2023-04-03 12:29:31 +0300

    WIP

--- resources/views/pages/rail/railservices.blade.php
+++ resources/views/pages/rail/railservices.blade.php
@@ -63,7 +63,7 @@
                                                     <li><a href="{{url('/road-transport')}}">Road Transport</a></li>
                                                     <li><a href="{{ url('/air-freight') }}">Air Freight</a></li>
                                                     <li><a href="{{ url('/ocean-freight') }}">Ocean Freight</a></li>
-                                                    <li><a href="#">Customs Clearance</a></li>
+                                                    <li><a href="{{ url('/customs-clearance') }}">Customs Clearance</a></li>
                                                     <li><a href="{{ url('/warehousing') }}">Warehousing</a></li>
                                                     <li><a href="#">Value Added Servives</a></li>
                                                     <li><a href="{{ url('/rail-services') }}">Railway Services</a></li>
@@ -100,7 +100,7 @@
                                                                 <li><a href="{{url('/road-transport')}}">Road Transport</a></li>
                                                                 <li><a href="{{url('/air-freight')}}">Air Freight</a></li>
                                                                 <li><a href="{{ url('/ocean-freight') }}">Ocean Freight</a></li>
-                                                                <li><a href="#">Customs Clearance</a></li>
+                                                                <li><a href="{{ url('/customs-clearance') }}">Customs Clearance</a></li>
                                                                 <li><a href="{{ url('/warehousing') }}">Warehousing</a></li>
                                                                 <li><a href="#">Value Added Servives</a></li>
                                                                 <li><a href="{{ url('/rail-services') }}">Railway Services</a></li>
@@ -177,7 +177,7 @@
                                     </li>
 
                                     <li class="menu-item">
-                                        <a href="#">Customs Clearance</a>
+                                        <a href="{{ url('/customs-clearance') }}">Customs Clearance</a>
                                     </li>
 
                                     <li class="menu-item">
