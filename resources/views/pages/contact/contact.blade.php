ef150962c560946b6cdb371ec33e49b294842da4
feisalmombo <feisalmombo29@gmail.com>
2023-06-06 01:31:26 +0300

    WIP

--- resources/views/pages/contact/contact.blade.php
+++ resources/views/pages/contact/contact.blade.php
@@ -69,9 +69,6 @@
                                                 </ul><!-- .dropdown-menu end -->
                                             </li><!-- .dropdown end -->
 
-
-											<li><a href="#">Media</a></li>
-
 											{{--  <li><a href="#">Terms & Conditions</a></li>  --}}
 
 											<li><a href="{{ url('/contact') }}">Contacts Us</a></li>
