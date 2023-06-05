f3ded33a3ff13c2bae59a02a8e7f23e4b0f23e15
feisalmombo <feisalmombo29@gmail.com>
2023-06-05 15:19:32 +0300

    WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 @if(Auth::user()->hasRole('developer') || Auth::user()->hasRole('staff'))
 <section class="content-header">
     <h1 style="font-family:Titillium Web, sans-serif">
-    Welcome To Harvest Logistics Company Limited Dashboard
+    Welcome To Oligarchy logistics Limited Dashboard
   </h1>
 </section>
 @endif
